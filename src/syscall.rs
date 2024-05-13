fd5e92df6da66bb9a02ec3a82ee50c7403b55d4e
Youjie Zheng <Azure_stars@126.com>
2024-05-13 11:42:44 +0800

    Update rust-toolchain to nightly-2024-05-02

--- src/syscall.rs
+++ src/syscall.rs
@@ -12,8 +12,8 @@ pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
             net_syscall_id, args
         );
 
-        #[allow(unused_assignments)]
-        ans = Some(crate::syscall_net::net_syscall(net_syscall_id, args));
+        (#[allow(unused_assignments)]
+        ans) = Some(crate::syscall_net::net_syscall(net_syscall_id, args));
     }
 
     if let Ok(mem_syscall_id) = crate::syscall_mem::MemSyscallId::try_from(syscall_id) {
@@ -21,8 +21,8 @@ pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
             "[syscall] id = {:#?}, args = {:?}, entry",
             mem_syscall_id, args
         );
-        #[allow(unused_assignments)]
-        ans = Some(crate::syscall_mem::mem_syscall(mem_syscall_id, args));
+        (#[allow(unused_assignments)]
+        ans) = Some(crate::syscall_mem::mem_syscall(mem_syscall_id, args));
     }
 
     if let Ok(fs_syscall_id) = crate::syscall_fs::FsSyscallId::try_from(syscall_id) {
@@ -30,8 +30,8 @@ pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
             "[syscall] id = {:#?}, args = {:?}, entry",
             fs_syscall_id, args
         );
-        #[allow(unused_assignments)]
-        ans = Some(crate::syscall_fs::fs_syscall(fs_syscall_id, args));
+        (#[allow(unused_assignments)]
+        ans) = Some(crate::syscall_fs::fs_syscall(fs_syscall_id, args));
     }
 
     if let Ok(task_syscall_id) = crate::syscall_task::TaskSyscallId::try_from(syscall_id) {
@@ -39,8 +39,8 @@ pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
             "[syscall] id = {:#?}, args = {:?}, entry",
             task_syscall_id, args
         );
-        #[allow(unused_assignments)]
-        ans = Some(crate::syscall_task::task_syscall(task_syscall_id, args));
+        (#[allow(unused_assignments)]
+        ans) = Some(crate::syscall_task::task_syscall(task_syscall_id, args));
     }
 
     if ans.is_none() {
